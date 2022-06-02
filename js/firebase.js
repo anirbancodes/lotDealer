8699da5fa52c382bb5bc6a1923c2c132bbd6c189
Anirban Deb <anirbandeb430@gmail.com>
2022-06-02 21:26:20 +0530

    2.0

--- js/firebase.js
+++ js/firebase.js
@@ -73,7 +73,7 @@ async function calcDrawTime() {
   let drawTime;
   if (gameHr == 12 && gameMin == 0 && ampm == "AM") ampm = "PM";
   if (gameHr < 9 && ampm == "AM") drawTime = "9:0 AM";
-  else if (gameHr > 9 && gameMin > 0 && ampm == "PM" && gameHr != 12)
+  else if (gameHr >= 9 && gameMin > 0 && ampm == "PM" && gameHr != 12)
     drawTime = "9:0 AM";
   else drawTime = gameHr + ":" + gameMin + " " + ampm;
   return { date, drawTime, time, gameHr, ampm, min, sec };
