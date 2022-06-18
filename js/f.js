da60f93a5a4f6d0e907681ea14ff29c663cfd76d
Anirban Deb <anirbandeb430@gmail.com>
2022-06-18 16:21:48 +0530

    2.0

--- js/f.js
+++ js/f.js
@@ -340,3 +340,115 @@ async function activateDealer(email, name) {
     console.error(e);
   }
 }
+
+const dtimes = [
+  "9:0 PM",
+  "8:45 PM",
+  "8:30 PM",
+  "8:15 PM",
+  "8:0 PM",
+  "7:45 PM",
+  "7:30 PM",
+  "7:15 PM",
+  "7:0 PM",
+  "6:45 PM",
+  "6:30 PM",
+  "6:15 PM",
+  "6:0 PM",
+  "5:45 PM",
+  "5:30 PM",
+  "5:15 PM",
+  "5:0 PM",
+  "4:45 PM",
+  "4:30 PM",
+  "4:15 PM",
+  "4:0 PM",
+  "3:45 PM",
+  "3:30 PM",
+  "3:15 PM",
+  "3:0 PM",
+  "2:45 PM",
+  "2:30 PM",
+  "2:15 PM",
+  "2:0 PM",
+  "1:45 PM",
+  "1:30 PM",
+  "1:15 PM",
+  "1:0 PM",
+  "12:45 PM",
+  "12:30 PM",
+  "12:15 PM",
+  "12:0 PM",
+  "11:45 AM",
+  "11:30 AM",
+  "11:15 AM",
+  "11:0 AM",
+  "10:45 AM",
+  "10:30 AM",
+  "10:15 AM",
+  "10:0 AM",
+  "9:45 AM",
+  "9:30 AM",
+  "9:15 AM",
+  "9:0 AM",
+];
+
+async function saleTbody() {
+  // const apiObj = fetchTime;
+  let date = fetchTime.date; //,apiTime = fetchTime.time;
+  if (!date) {
+    let now = new Date();
+    let date1 =
+      now.getFullYear() + "-" + (now.getMonth() + 1) + "-" + now.getDate();
+    date = date1;
+  }
+  // if (date == apiDate) {
+  //   //today
+  // }
+  // document.getElementById("today").innerHTML = date;
+  const ref = doc(db, "result", date);
+  const docSnap = await getDoc(ref);
+  if (docSnap.exists()) {
+    const result = docSnap.data();
+    document.getElementById(
+      "sale-tbody"
+    ).innerHTML = `<li class="table-header li">
+            <div class="col2">Time</div>
+            <div class="col2">Lucky No.</div>
+          </li>`;
+    // let keys = Object.keys(result);
+    // keys.forEach((dtime) => {
+    dtimes.forEach((dtime) => {
+      if (result[dtime] != undefined || result[dtime] != null)
+        document.getElementById("sale-tbody").innerHTML +=
+          `<li class="table-row li">
+      <div class="col2">` +
+          dtime +
+          `</div>
+      <div class="col2 or2" ><strong>` +
+          result[dtime] +
+          `</strong></div>
+    </li>`;
+    });
+  } else {
+    document.getElementById("sale-tbody").innerHTML = "No Permission";
+  }
+}
+saleTbody();
+const refreshResBtn = document.getElementById("refreshResBtn");
+refreshResBtn.addEventListener("click", () => {
+  let date = document.getElementById("date").value;
+  if (date) {
+    let i1 = date.indexOf("-"),
+      i2 = date.lastIndexOf("-");
+    date =
+      date.substring(0, i1 + 1) +
+      (Number(date.substring(i1 + 1, i2)) / 10) * 10 +
+      "-" +
+      (Number(date.substring(i2 + 1, i2 + 3)) / 10) * 10;
+  } else if (!date) {
+    let now = new Date();
+    date = now.getFullYear() + "-" + (now.getMonth() + 1) + "-" + now.getDate();
+  }
+  saleTbody(date);
+});
